9e3398d7e1277e9aeed7f8fce0f820b338e71fc1
wzp <zzzwp_wzp@163.com>
2017-12-25 21:12:47 +0800

    update file 02

--- src/com/wzp/pojo/Test01.java
+++ src/com/wzp/pojo/Test01.java
@@ -4,8 +4,8 @@ public class Test01 {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("hello,github·şÎñÆ÷");
-		System.out.println("hello,github·şÎñÆ÷02");
+		System.out.println("hello,githubæœåŠ¡å™¨");
+		System.out.println("hello,githubæœåŠ¡å™¨02");
 		
 	}
 
